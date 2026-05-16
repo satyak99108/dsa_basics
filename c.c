249f0551afb1e9dc0b2e4459faa3c4e47ef23ebf
Satyak <satyak99108@gmail.com>
2026-05-16 08:28:00 +0530

    initial commit

--- c.c
+++ c.c
@@ -3,6 +3,32 @@
 
 struct Node{
     int data;
-    struct node* next;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+struct Node* createNode(int data){
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
 }
 
+void ins_beg(int value){
+    struct Node* new_node = createNode(value);
+    new_node->next = head;
+    head = new_node;
+}
+
+void ins_end(int value){
+    struct Node* new_node = createNode(value);
+
+    struct Node* temp = head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+
+    temp->next = head;
+
+}
